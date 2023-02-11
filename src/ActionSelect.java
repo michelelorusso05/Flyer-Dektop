0b42d39481b564f79e16c9686314837438296230
Michele Lorusso <michelex2005@gmail.com>
2023-02-11 09:02:30 +0100

    Created form

--- src/ActionSelect.java
+++ src/ActionSelect.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
+
 public class ActionSelect {
+    private JButton button1;
+    private JPanel panel1;
+
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        JFrame frame = new JFrame("ActionSelect");
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        frame.setContentPane(new ActionSelect().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
