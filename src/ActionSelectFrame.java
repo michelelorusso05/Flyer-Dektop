0d8af9472c7a2ecf37366f08cca164e1ce054ae0
gabri <gabrielecoco0@mail.com>
2023-02-23 08:42:39 +0100

    ersiumme

--- src/ActionSelectFrame.java
+++ src/ActionSelectFrame.java
@@ -1,11 +1,19 @@
+import com.formdev.flatlaf.FlatDarkLaf;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
 public class ActionSelectFrame extends JFrame {
     public ActionSelectFrame() throws HeadlessException {
         super("Flyer");
+        FlatDarkLaf.setup();
+
+        UIManager.put( "Button.arc", 50 );
+        UIManager.put( "Component.arc", 50 );
+        UIManager.put( "ProgressBar.arc", 50 );
+        UIManager.put( "TextComponent.arc", 50 );
+
         setBounds(0, 0, 800, 600);
         setLocationRelativeTo(null);
         addWindowListener(new WindowEventManager());
